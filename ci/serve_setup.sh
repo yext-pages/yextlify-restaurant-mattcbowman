6be2c68bd1882b7b094581b29975b67e7e00cb94
mbowman <mbowman@yext.com>
2020-09-09 23:43:18 -0400

    Update buid setup and serve setup scripts

--- ci/serve_setup.sh
+++ ci/serve_setup.sh
@@ -1,3 +1,4 @@
-#!bin/bash
+#!bin/sh
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.74.3/hugo_0.74.3_Linux-32bit.tar.gz | tar -xz /usr/local/bin
 npm install
+npm install -g gulp-cli
